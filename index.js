7394fd51d95220c408f13517637aa03d82f60db3
Charles <Charlessammarco@gmail.com>
2023-05-23 11:15:23 -0500

    sending data

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+// // Add your code here
+function submitData(firstName, email){
+  const configurationObject={
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      "name": firstName,
+      "email": email,
+    })
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(res=>res.json())
+  .then(obj=>{
+    let body = document.querySelector("body")
+    body.append(obj.id)
+  })
+  
+  .catch(function(error) {
+    alert("Unauthorized Access");
+    console.log(error.message);
+    let body = document.querySelector("body")
+    body.append(error.message)
+    })
+  }
+  
+
+  
+
+
+
+
+
+
